b49ff2123f3600e311370d0f193eda8f123ce357
Devin Warrick <dev.warr@gmail.com>
2019-11-11 16:17:56 -0600

    New errorHandler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -11,15 +11,27 @@ module.exports = (err, req, res, next) => {
     };
 
     if (errObj.status === 500) {
-    // A Nice Consoled Message
-    console.log(new Error(errObj.devMessage || errObj.details).stack
-                .replace(/(?<=\n\s+)(.*)(?=\n\s)/g, match => {
-                    if (!match.includes("node_modules")) {
-                        return `\x1b[31m${match}\x1b[0m`;
-                    } else {
-                        return match;
-                    }
-                }));
+    // A Nice Consoled Message:
+    // Create an Error and grab the stack. Add a "\n" because...
+    const stackTrace = new Error(errObj.devMessage || errObj.details).stack + "\n";
+
+    // ...we need some regex! We look through each line of our 
+    //     stackTrace, find any lines that DON'T include "node_modules"
+    //     (which means they were actually coded out), and then we 
+    //     highlight those specific lines in red
+    //     Note: \x1b[31m = red font color
+    //           \x1b[0m  = reset color
+    const markedStackTrace = 
+        stackTrace
+            .replace(/(?<=\n\s+)(.*)(?=\n)/g, match => {
+                if (!match.includes("node_modules")) {
+                    return `\x1b[31m${match}\x1b[0m`;
+                } else {
+                    return match;
+                }
+            })
+
+    console.log(markedStackTrace);
     }
 
     res.status(errObj.status).json(errObj);
